9a839573150395a4c4fd2063d63ac3817b7d0aa3
Zopa2222 <s.medina08@ufromail.cl>
2023-03-23 10:06:42 -0300

    ingresarDatos arreglado

--- src/Main.java
+++ src/Main.java
@@ -13,21 +13,21 @@ public class Main {
             System.out.println("Ingrese 1 para ingresar nuevos datos, 2 para ver el sismo de mayor intesidad, 3 para ver la cantidad de sismos mayores a 5.0, 4 para simular el envío de SMS, 5 para salir");
             String opcion = in.next();
             if (Objects.equals(opcion, "1")){
-                ingresarDatos();
+                double[][] listaDias = ingresarDatos();
+
             }
         }
     }
-    public static void ingresarDatos(){
-        ArrayList listaDias = new ArrayList<>();
-        for (int i=1;i<8;i++){
-            ArrayList numDia = new ArrayList<>();
-            for (int j=1;j<25;i++) {
+    public static double[][] ingresarDatos(){
+        double[][] listaDias = new double[7][24];
+        for (int i=0;i< listaDias.length;i++){
+            for (int j=0;j<listaDias[0].length;j++) {
                 double sismo = Math.random()*9.9;
-                numDia.add(sismo);
+                listaDias[i][j]=sismo;
             }
-            listaDias.add(numDia);
         }
         System.out.println(listaDias);
+        return listaDias;
 
     }
 }
