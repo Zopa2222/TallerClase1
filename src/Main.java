6fdf3adfc9f93f935091fd0002c7416dd31b476a
Zopa2222 <s.medina08@ufromail.cl>
2023-03-23 09:01:14 -0300

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void menu() {
+        a=0;
+        while (a==0){
+            System.out.println("Ingrese 1 para ingresar nuevos datos, 2 para ver el sismo de mayor intesidad, 3 para ver la cantidad de sismos mayores a 5.0, 4 para simular el envío de SMS, 5 para salir");
+            Scanner sc= new Scanner(System.in);
+            String opcion=sc;
+            if (opcion==1){
+                ingresarDatos()
+            }
+        }
+    }
+    public static void ingresarDatos(){
+        ArrayList listaDias =[]
+    }
+}
